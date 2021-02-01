7b1fa6025706432e9f54d145a7d6a721e5dbb8b2
David Angeles Ungson <davidangelesu@gmail.com>
2021-02-01 22:17:04 +0100

    Documented Polygon Header File

--- include/Polygon.h
+++ include/Polygon.h
@@ -9,8 +9,22 @@ namespace GUI {
 	struct Point { double x; double y; };
 	class Polygon {
 	public:
+		/**
+		 * @brief  Evaluates if point is inside the Polygon
+		 * @param x x-Coordinate
+		 * @param y y-Coordinate
+		 * @return if point is inside Polygon
+		*/
 		bool isInside(double x, double y);
+		/**
+		 * @brief Adds a vertex to the list of vertices that define the polygon
+		 * @param x x-Coordinate
+		 * @param y y-Coordinate
+		*/
 		inline void addVertex(double x, double y) { m_vertices.push_back(Point{ x,y }); };
+		/**
+		 * @brief Clears the list of vertices
+		*/
 		inline void clearVertices() { m_vertices.clear(); };
 	private:
 		std::vector<Point> m_vertices;
