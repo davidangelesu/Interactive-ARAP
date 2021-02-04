05a78878602fd1a83b8802da4d114cf766435e16
David Angeles Ungson <davidangelesu@gmail.com>
2021-02-04 12:12:43 +0100

    Update polygon.h

--- include/polygon.h
+++ include/polygon.h
@@ -6,11 +6,25 @@
 // Code to check if a given point lies inside a given polygon 
 // Based on https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/ 
 namespace GUI {
-	struct Point { int x; int y; };
+	struct Point { double x; double y; };
 	class Polygon {
 	public:
-		bool isInside(int x, int y);
-		inline void addVertex(int x, int y) { m_vertices.push_back(Point{ x,y }); };
+		/**
+		 * @brief  Evaluates if point is inside the Polygon
+		 * @param x x-Coordinate
+		 * @param y y-Coordinate
+		 * @return if point is inside Polygon
+		*/
+		bool isInside(double x, double y);
+		/**
+		 * @brief Adds a vertex to the list of vertices that define the polygon
+		 * @param x x-Coordinate
+		 * @param y y-Coordinate
+		*/
+		inline void addVertex(double x, double y) { m_vertices.push_back(Point{ x,y }); };
+		/**
+		 * @brief Clears the list of vertices
+		*/
 		inline void clearVertices() { m_vertices.clear(); };
 	private:
 		std::vector<Point> m_vertices;
